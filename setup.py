660f76f9d2b66e5df6f0a35cad609860f21fe6c8
AndreiRoibu <aroibu1@gmail.com>
2021-04-28 17:37:43 +0100

    updated setup name and description

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Linear Regression',
+    name='Logistic Regression',
     version='0.0.1',
-    description='A series of basic linear regression files',
+    description='A series of basic logistic regression files',
     license='BSD 3-clause license',
     maintainer='AndreiRoibu',
     maintainer_email='aroibu1@gmail.com',
